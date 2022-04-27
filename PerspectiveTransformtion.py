ab3eef1a8da122b364453d4a081fa28267e182c4
Mansi74 <54806210+Mansi74@users.noreply.github.com>
2022-04-27 12:34:17 +0200

    Update PerspectiveTransformation

--- PerspectiveTransformtion.py
+++ PerspectiveTransformtion.py
@@ -3,12 +3,6 @@ import numpy as np
 
 class PerspectiveTransformation:
     """ This a class for transforming image between front view and top view
-
-    Attributes:
-        src (np.array): Coordinates of 4 source points
-        dst (np.array): Coordinates of 4 destination points
-        M (np.array): Matrix to transform image from front view to top view
-        M_inv (np.array): Matrix to transform image from top view to front view
     """
     def __init__(self):
         """Init PerspectiveTransformation."""
@@ -25,26 +19,14 @@ class PerspectiveTransformation:
 
     def forward(self, img, img_size=(1280, 720), flags=cv2.INTER_LINEAR):
         """ Take a front view image and transform to top view
-
-        Parameters:
-            img (np.array): A front view image
-            img_size (tuple): Size of the image (width, height)
-            flags : flag to use in cv2.warpPerspective()
-
-        Returns:
+        Output:
             Image (np.array): Top view image
         """
         return cv2.warpPerspective(img, self.M, img_size, flags=flags)
 
     def backward(self, img, img_size=(1280, 720), flags=cv2.INTER_LINEAR):
         """ Take a top view image and transform it to front view
-
-        Parameters:
-            img (np.array): A top view image
-            img_size (tuple): Size of the image (width, height)
-            flags (int): flag to use in cv2.warpPerspective()
-
-        Returns:
+        Output:
             Image (np.array): Front view image
         """
         return cv2.warpPerspective(img, self.M_inv, img_size, flags=flags)
